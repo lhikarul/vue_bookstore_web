99be528d64efed48867b6c31b511fc953b26010b
lhikarul <711hikaru@gmail.com>
2019-10-24 21:19:46 +0800

    3-2 翻頁功能實現

--- src/views/Ebook.vue
+++ src/views/Ebook.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="ebook">
-        <div id="read"></div>
+        <div class="read-wrapper">
+            <div id="read"></div>
+            <div class="mask">
+                <div class="left" @click="prevPage"></div>
+                <div class="center"></div>
+                <div class="right" @click="nextPage"></div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -24,6 +31,18 @@ export default {
 
             // 渲染電子書
             this.rendition.display();
+        },
+        prevPage () {
+            if (this.rendition) {
+                console.log('hello')
+                this.rendition.prev();
+            }
+        },
+        nextPage () {
+            if (this.rendition) {
+                console.log('next')
+                this.rendition.next();
+            }
         }
     },
     mounted () {
@@ -32,3 +51,34 @@ export default {
 
 }
 </script>
+
+<style lang="scss" scoped>
+    @import 'assets/css/global';
+
+    .ebook {
+        position: relative;
+        .read-wrapper {
+            .mask {
+                display: flex;
+                position: absolute;
+                top: 0;
+                left: 0;
+                z-index: 100;
+                width: 100%;
+                height: 100%;
+
+                .left {
+                    flex: 0 0 px2rem(100);
+                }
+
+                .center {
+                    flex: 1;
+                }
+
+                .right {
+                    flex: 0 0 px2rem(100);
+                }
+            }
+        }
+    }
+</style>
