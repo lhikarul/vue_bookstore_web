0729ad6193f5fad611e42a93fe817a027e87b2a2
lhikarul <711hikaru@gmail.com>
2019-10-24 21:05:09 +0800

    快速入門 3-2 電子書解析和渲染

---
+++ src/views/Ebook.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="ebook">
+        <div id="read"></div>
+    </div>
+</template>
+
+<script>
+import Epub from 'epubjs';
+
+const DOWNLOAD_URL = '/2018_Book_AgileProcessesInSoftwareEngine.epub';
+
+export default {
+    name: 'Ebook',
+    methods: {
+        showEpub () {
+            // 生成 book
+            this.book = new Epub(DOWNLOAD_URL);
+
+            // 生成 rendition
+            this.rendition = this.book.renderTo('read', {
+                width: window.innerWidth,
+                height: window.innerHeight
+            })
+
+            // 渲染電子書
+            this.rendition.display();
+        }
+    },
+    mounted () {
+        this.showEpub()
+    }
+
+}
+</script>
