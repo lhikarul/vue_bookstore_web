f4c0761e99b057c08624d73d123e6aa6e4eab040
Evans <711hikaru@gmail.com>
2019-11-08 22:27:11 +0800

    5-7 tab 選項切換和搜尋效果

--- src/components/ebook/EbookSlide.vue
+++ src/components/ebook/EbookSlide.vue
@@ -4,8 +4,13 @@
       <transition name="slide-right">
       <div class="content">
           <div class="content-page-wrapper">
-              <div class="content-page"></div>
-              <div class="content-page-tab"></div>
+              <div class="content-page">
+                  <component :is="currentTab === 1 ? content : bookmark"></component>
+              </div>
+              <div class="content-page-tab">
+                  <div class="content-page-tab-item" :class="{'selected': currentTab === 1}" @click="selectTab(1)">{{$t('book.navigation')}}</div>
+                  <div class="content-page-tab-item" :class="{'selected': currentTab === 2}" @click="selectTab(2)">{{$t('book.bookmark')}}</div>
+              </div>
           </div>
       </div>
       </transition>
@@ -16,18 +21,28 @@
 
 <script>
 import {ebookMixin} from 'utils/mixin';
+import EbookSlideContent from './EbookSlideContent';
+
 export default {    
     name: 'EbookSlide',
     mixins: [ebookMixin],
+    data () {
+        return {
+            currentTab: 1,
+            content: EbookSlideContent,
+            bookmark: null
+        }
+    },
     methods: {
-        hide () {
-            console.log('click')
+        selectTab (tab) {
+            this.currentTab = tab;
         }
     }
 }
 </script>
 
 <style lang="scss" scoped>
+@import 'assets/css/global';
     .slide-content-wrapper {
         position: absolute;
         top: 0;
@@ -40,6 +55,28 @@ export default {
             flex: 0 0 85%;
             width: 85%;
             height: 100%;
+            .content-page-wrapper {
+                display: flex;
+                flex-direction: column;
+                width: 100%;
+                height: 100%;
+                .content-page {
+                    flex: 1;
+                    width: 100%;
+                    overflow: hidden;
+                }
+                .content-page-tab {
+                    display: flex;
+                    flex: 0 0 px2rem(48);
+                    width: 100%;
+                    height: px2rem(48);
+                    &-item {
+                        flex: 1;
+                        font-size: px2rem(12);
+                        @include center;
+                    }
+                }
+            }
         }
         .content-bg {
             flex: 0 0 15%;
---
+++ src/components/ebook/EbookSlideContent.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+      <div class="ebook-slide-contents">
+          <div class="slide-contents-search-wrapper">
+              <div class="slide-contents-search-input-wrapper">
+                  <div class="slide-contents-search-icon">
+                      <span class="icon-search"></span>
+                  </div>
+                  <input type="text" class="slide-contents-search-input" 
+                        :placeholder="$t('book.searchHint')"
+                        @click="showSearchPage "
+                        >
+              </div>
+              <div class="slide-contents-search-cancel" v-if="searchVisible" @click="hideSearchPage()">{{$t('book.cancel')}}</div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import {ebookMixin} from 'utils/mixin';
+export default {
+    name: 'EbookSlideContent',
+    mixins: [ebookMixin],
+    data () {
+        return {
+            searchVisible: false
+        }
+    },
+    methods: {
+        hideSearchPage () {
+            this.searchVisible = false;
+        },
+        showSearchPage () {
+            this.searchVisible = true;
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'assets/css/global';
+.ebook-slide-contents {
+    width: 100%;
+    .slide-contents-search-wrapper {
+        margin: px2rem(20) 0 px2rem(10) 0;
+        padding: 0 px2rem(15);
+        box-sizing: border-box;
+        display: flex;
+        width: 100%;
+        height: px2rem(36);
+        .slide-contents-search-input-wrapper {
+            flex: 1;
+            @include center;
+            .slide-contents-search-icon {
+                flex: 0 0 px2rem(28);
+                font-size: px2rem(12);
+                @include center;
+            }
+            .slide-contents-search-input {
+                flex: 1;
+                width: 100%;
+                height: px2rem(32);
+                font-size: px2rem(14);
+                background: transparent;
+                border: none;
+                &:focus {
+                    outline: none;
+                }
+            }
+        }
+        .slide-contents-search-cancel {
+            flex: 0 0 px2rem(50);
+            font-size: px2rem(14);
+            @include right;
+        }
+    }
+}
+</style>
